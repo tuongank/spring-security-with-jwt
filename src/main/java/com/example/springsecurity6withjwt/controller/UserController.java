825ba3d4631b1ed2e63da461a0528bf8561a5665
Tuong Anh <98013554+Tuoank@users.noreply.github.com>
2024-12-02 09:25:56 +0700

    dang nhap, dang ky

--- src/main/java/com/example/springsecurity6withjwt/controller/UserController.java
+++ src/main/java/com/example/springsecurity6withjwt/controller/UserController.java
@@ -1,26 +1,34 @@
 package com.example.springsecurity6withjwt.controller;
 
 import com.example.springsecurity6withjwt.dto.ApiResponse;
-import com.example.springsecurity6withjwt.dto.LoginRequest;
-import com.example.springsecurity6withjwt.dto.RegisterRequest;
+import com.example.springsecurity6withjwt.dto.UpdateRequest;
+import com.example.springsecurity6withjwt.entity.User;
 import com.example.springsecurity6withjwt.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
 @RequiredArgsConstructor
+@RestController
 @RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/log-in")
-    public ResponseEntity<ApiResponse> login(@RequestBody LoginRequest loginRequest) {
-        return ResponseEntity.ok(userService.login(loginRequest));
+    @PutMapping("/update")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<ApiResponse> update(@RequestBody UpdateRequest updateRequest) {
+        return ResponseEntity.ok(userService.update(updateRequest));
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<ApiResponse> register(@RequestBody RegisterRequest registerRequest) {
-        return ResponseEntity.ok(userService.register(registerRequest));
+    @GetMapping("/my-info")
+    public ResponseEntity<User> myInfo() {
+        return ResponseEntity.ok(userService.myInfor());
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<ApiResponse> getAll() {
+        return ResponseEntity.ok(userService.getAll());
     }
 }
