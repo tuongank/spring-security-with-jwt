32944a86fa1d69beaebab7b4fa6fe72a0946c679
Tuong Anh <98013554+Tuoank@users.noreply.github.com>
2024-12-02 09:25:19 +0700

    dang nhap, dang ky

---
+++ src/main/java/com/example/springsecurity6withjwt/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.springsecurity6withjwt.controller;
+
+import com.example.springsecurity6withjwt.dto.ApiResponse;
+import com.example.springsecurity6withjwt.dto.LoginRequest;
+import com.example.springsecurity6withjwt.dto.RegisterRequest;
+import com.example.springsecurity6withjwt.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final UserService userService;
+
+    @PostMapping("/log-in")
+    public ResponseEntity<ApiResponse> login(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(userService.login(loginRequest));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(userService.register(registerRequest));
+    }
+}
